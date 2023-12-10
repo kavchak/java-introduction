1ac42b3a2621286e415ee1cb046153988a68d107
kavchak <kavchakvova@gmail.com>
2023-12-10 20:35:52 +0200

    classwork: lesson 3 and remove .idea folder

---
+++ src/com/hillel/classwork/lesson3/FitnessTracker.java
@@ -0,0 +1,12 @@
+package com.hillel.classwork.lesson3;
+
+public class FitnessTracker {
+    public static void main(String[] args) {
+
+        int distance = 10;
+        int time = 12;
+
+        float result = ((float) distance / time);
+        System.out.println("Человек прошел в час: " + result + " тысяч часов");
+    }
+}
