ba11302c3a74df1cbecd5c9dbaf826a91c48676a
kavchak <kavchakvova@gmail.com>
2024-03-01 19:00:55 +0200

    classwork: lesson 10

---
+++ src/com/hillel/classwork/lesson10/Fill2dArray.java
@@ -0,0 +1,20 @@
+package com.hillel.classwork.lesson10;
+
+public class Fill2dArray {
+
+    public static void main(String[] args) {
+
+        int [][] arr = new int[5][5];
+
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i][i] = 1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
